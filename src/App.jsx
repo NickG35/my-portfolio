4f1ba481f93e061dfbebc0fde7b03d865c192433
NickG35 <ngeorge356@gmail.com>
2025-05-21 14:03:38 -0400

    got rid of unnecessary imports and left scrollspy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Home from './components/Home';
-import Projects from './components/Projects';
+import ScrollSpy from './components/ScrollSpy';
 import './App.css';
 
 function App() {
   return (
       <div className="App">
-        <Home />
-        <Projects />
+        <ScrollSpy />
       </div> 
   );
 }
