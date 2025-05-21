a9da97de64e9f0c41036853a3ee190125e9b8133
NickG35 <ngeorge356@gmail.com>
2025-05-21 14:04:14 -0400

    added class section and project placeholder

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,11 @@
 function Projects(){
     return (
-        <section>
+        <section className="portfolio-section">
             <h2>Projects</h2>
             <ul>
                 <li>Weather App</li>
                 <li>E-commerce site</li>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dolorum fuga modi vel illo reiciendis voluptatibus, impedit minima magni id in nam assumenda nesciunt. Optio non eaque porro aliquid veniam?</p>
             </ul>
         </section>
     );
