93c9377af24ac7ba09b1b6e475f61d31331f1ee6
NickG35 <ngeorge356@gmail.com>
2025-05-22 14:05:05 -0400

    initiated experience section

---
+++ src/components/Experience.jsx
@@ -0,0 +1,8 @@
+function Experience(){
+    return(
+        <section className="portfolio-section">
+            <p>Here are all my Experiences</p>
+        </section>
+    );
+}
+export default Experience;
