fe4ee3cfea2eaf96262c61c79d61f4b310dcfa9c
NickG35 <ngeorge356@gmail.com>
2025-05-22 14:05:38 -0400

    implemented title, profile pic, name, bio, and resume download placeholder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
+import myImage from "../assets/profile_pic.JPEG"
 function Home(){
     return (
         <section className="portfolio-section">
-            <h1>Hi, I'm Nick</h1>
-            <p>Welcome to my portfolio</p>
+            <h1>Full Stack Developer</h1>
+            <div className="image-holder">
+                <img src={myImage} className="profile_pic"/> 
+            </div>
+            <h1>Nicholas George</h1>
+            <p>Full Stack Developer based in Philadelphia with 2.5+ years of hands-on experience building modern web apps. Self-taught, driven, and ready to contribute to real-world teams while continuing to grow.</p>
+            <button>Download Resume</button>
         </section>
     );
 }
