8ef17dc4208b738544a5ea5ede62d33ca5e052d1
NickG35 <ngeorge356@gmail.com>
2025-05-22 14:06:09 -0400

    initiated services page

---
+++ src/components/Services.jsx
@@ -0,0 +1,8 @@
+function Services(){
+    return(
+        <section className="portfolio-section">
+            <p>Here are all my services</p>
+        </section>
+    );
+}
+export default Services;
