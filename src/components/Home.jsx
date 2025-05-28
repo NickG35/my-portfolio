baa6914b333f04ff7790941f1449ead6f033e027
NickG35 <ngeorge356@gmail.com>
2025-05-28 11:38:41 -0400

    Adjusted home page layout and implemented contact info div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,24 @@ import myImage from "../assets/profile_pic.JPEG"
 function Home(){
     return (
         <section className="portfolio-section">
-            <h1>Full Stack Developer</h1>
-            <div className="image-holder">
-                <img src={myImage} className="profile_pic"/> 
+            <div className="profile-container">
+                <h1 className="header">Full Stack Developer</h1>
+                <div className='profile-display'>
+                    <div><h1 className="header">Nicholas</h1></div>
+                    <div className="image-holder">
+                        <img src={myImage} className="profile_pic"/> 
+                    </div>
+                    <div><h1 className="header">George</h1></div>
+                </div>
+                <div className="contact-info">
+                    <button>Resume</button>
+                    <img className="contact-links" src="src/assets/linkedin-logo-linkedin-icon-transparent-free-png.webp"></img>
+                    <img id="mail-link" className="contact-links" src="src/assets/gmail-new.png"></img>
+                </div>
+                <div className="bio">
+                    <p>Full Stack Developer based in Philadelphia with 2.5+ years of hands-on experience building modern web apps. Self-taught, driven, and ready to contribute to real-world teams while continuing to grow.</p>
+                </div>
             </div>
-            <h1>Nicholas George</h1>
-            <p>Full Stack Developer based in Philadelphia with 2.5+ years of hands-on experience building modern web apps. Self-taught, driven, and ready to contribute to real-world teams while continuing to grow.</p>
-            <button>Download Resume</button>
         </section>
     );
 }
