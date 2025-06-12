4c0ebfe405a8ba379a666cbb42d5f831e1b8c7ba
NickG35 <ngeorge356@gmail.com>
2025-06-12 16:09:42 -0400

    implemented services description

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ function Services(){
     return(
         <section className="portfolio-section">
             <h2>Services</h2>
-            <p> I build full-stack web apps using Python, JavaScript, and modern frameworks like Django, React, and Flask. With experience of building applications such as weather forecasting, clothing storefront, and  prescription management, I focus on performance, usability, and clean code.</p>
+            <p> I build full-stack web apps using Python, JavaScript, and modern frameworks like Django, React, and Flask. With experience of building applications that include weather forecasting, a clothing storefront, and  pharmacy management, I focus on performance, usability, and clean code.</p>
         </section>
     );
 }
